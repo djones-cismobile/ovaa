e772faba998c78ca125182ea0ac7014e40c1e370
David A. Jones <djones@cismobile.com>
2022-08-09 18:54:34 -0400

    Update WeakCrypto.java
    attempt#1 to remove hardcoded key

--- app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
+++ app/src/main/java/oversecured/ovaa/utils/WeakCrypto.java
@@ -6,11 +6,15 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class WeakCrypto {
-    private static final String KEY = "49u5gh249gh24985ghf429gh4ch8f23f";
+    private static final String KEY = getKey();
 
     private WeakCrypto() {
     }
-
+    
+    private String getKey() {
+           return ("49u5gh249gh24985ghf429gh4ch8f23f");
+    }
+    
     public static String encrypt(String data) {
         try {
             SecretKeySpec secretKeySpec = new SecretKeySpec(KEY.getBytes(), "AES");
